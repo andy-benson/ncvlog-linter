d9a0ce2a5ad5bd4762a6d7f7b0f3955675d44ddc
Andy Benson <andy.benson@arm.com>
2020-08-24 17:35:37 +0100

    some more changes

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 ## ncvlog-linter package
 
 
-systemverilog linter forked from :  https://github.com/KoenGoe/atom-vcs-linter)
+SystemVerilog linter forked from :  https://github.com/KoenGoe/atom-vcs-linter)
 
-Requires 'ncvlog' (Cadence Incisive) to compile a single verilog file, that gives more insightful error messages than VCS. 
+Requires 'ncvlog' (Cadence Incisive) to compile the current verilog file. Cadence Incisive , seems to give the most "incisive"  error messages - hence the fork from atom-vcs-linter
 
 Keeping the ncvlog compile to a single file keeps run time super speedy, and therefore the linter can work in-line during editing  ( you might want https://atom.io/packages/autosave-onchange)
 
@@ -12,16 +12,41 @@ This downside to this approach, is that project-wide dependencies such as  'incl
 ### Dependencies :
 
 1. 'ncvlog' available in your path
-1.https://atom.io/packages/linter
+1. https://atom.io/packages/linter
 
 ### Under the hood
 
-ncvlog-linter runs the following command line each time the current file is saved:
+ncvlog-linter runs the following command line each time the current file (i.e. lint_test.v) is saved
 
 ```
-ncvlog -sv +incdir+<pwd> <myfile>
+ncvlog -sv +incdir+. -work mylib -logfile /tmp/logfile lint_test.v
 ```
-Each of the error messages are then parsed , and reformatted before being passed back to the linter package. 
+
+mylib is set through the following 2 files :
+
+```
+#cds.lib
+ DEFINE mylib /tmp/mylib 
+```
+
+```
+#hdl.var
+DEFINE WORK mylib 
+```
+
+
+Each of the error messages are then parsed , and reformatted before being passed back to the linter package. I.e. :  
+
+Output from ncvlog 
+
+```
+ncvlog: 15.20-s060: (c) Copyright 1995-2018 Cadence Design Systems, Inc.
+endmodule 
+        |
+ncvlog: *E,EXPLPA (lint_test.v,6|8): expecting a left parenthesis ('(') [12.1.2][7.1(IEEE)].
+```
+
+input to Atom linter
 
 ```
 lint_test.sv:14:Error:expecting an identifier [9.2(IEEE)].
@@ -30,5 +55,3 @@ lint_test.sv:14:Error:expecting an identifier [9.2(IEEE)].
 ### ToDo
 
 Add menu item so that additional command line options can be passed to ncvlog, such as additional files or include directories. 
-
-
--- lib/core_ncvlog.py
+++ lib/core_ncvlog.py
@@ -20,8 +20,7 @@ import subprocess
 
 import os
 import re
-import shutil
-import uuid
+
 
 args = sys.argv
 filelist=args[1:]
@@ -43,17 +42,28 @@ def process_line(msg_type,line):
     
     matches = re.search(r"\(([\w\.\/-]+),(\d*)\|\d*\):\s(.+\.$)", my_line)
 
-    file     = str(matches.group(1))
-    line_num = str(matches.group(2))
-    message  = str(matches.group(3))
+    if (matches):
+        file     = str(matches.group(1))
+        line_num = str(matches.group(2))
+        message  = str(matches.group(3))
 
-    return file +":"+ line_num +":"+ msg_type +":"+ message + "\n\n"
-
+        return file +":"+ line_num +":"+ msg_type +":"+ message + "\n\n"
+    
+    # if there no match something has gone wrong with the regex, put error on first line 
+    else:    
+        return str(filelist[0]) +":0:Error:"+ my_line + "\n\n" 
+      
 #-----------------------------------------------------------------------------
 # run ncvlog on the current file
+#
+# work libs and logfiles are not required so directed to the black-hole of /tmp
+#
+# command line also needs cds.lib and hdl.var files which specify the worklib
+#
+# Todo : could add user defined temp directory if /tmp is not suitable
 #-----------------------------------------------------------------------------
 
-ncvlog_output = subprocess.run(["ncvlog", incdir_cmd, "-sv", str(filelist[0])],capture_output=True,universal_newlines=True)
+ncvlog_output = subprocess.run(["ncvlog", incdir_cmd, "-sv", "-work", "mylib", "-logfile", "/tmp/logfile",str(filelist[0])], capture_output=True,universal_newlines=True)
 
 lines = ncvlog_output.stdout
 
@@ -63,10 +73,7 @@ lines = lines.split("\n")
 #-----------------------------------------------------------------------------
 # parse each of the lines looking for the ncvlog info / warning / error codes
 #
-# also can filter out certain error codes if required
-# TODO : could 
 #----------------------------------------------------------------------------
-
 
 for line in lines:
 
@@ -80,54 +87,10 @@ for line in lines:
             out += process_line("Info",line)
 
 #-----------------------------------------------------------------------------
-# clean up intermediate files and return output to providelinter function 
-#-----------------------------------------------------------------------------        
-
-if os.path.isfile("ncvlog.log"):
-    os.remove("ncvlog.log")
-
-# getting some weird error "Directroy not empty when running shutil.rmtree("INCA_libs")
-# only when executed by the atom-ncvlog-linter.js. Think it is to do with process
-# permissions ? anyway brute force with platfrom specific code
-# subprocess.run(["rm", "-fr", "INCA_libs"])
-# update
-# this still causes problems ( see below) so as another brute force approach mv the pesky 
-# directory into /tmp with a unique identifier. 
-
-if os.path.isdir("INCA_libs"):
-    shutil.move("INCA_libs","/tmp/" + str(uuid.uuid1))
+# return formatted errors back to calling java script
+#
+#----------------------------------------------------------------------------
 
 print(out)
 
-sys.exit(0)
-
-#-----------------------------------------------------------------------------
-# ncvlog errors
-#-----------------------------------------------------------------------------
-
-
-# csi-ncvlog - CSI: Command line:
-# ncvlog
-#     +incdir+/home/abenson/.atom/packages/atom-ncvlog-linter/lib
-#     -sv
-#     /home/abenson/.atom/packages/atom-ncvlog-linter/lib/muscab2_uvm_tb_top.sv
-#     /home/abenson/.atom/packages/atom-ncvlog-linter/lib
-# 
-# csi-ncvlog - CSI: *F,INTERR: INTERNAL EXCEPTION
-# -----------------------------------------------------------------
-# The tool has encountered an unexpected condition and must exit.
-# Contact Cadence Design Systems customer support about this
-# problem and provide enough information to help us reproduce it,
-# including the logfile that contains this error message.
-#   TOOL:	ncvlog	15.20-s060
-#   HOSTNAME: login18.euhpc.arm.com
-#   OPERATING SYSTEM: Linux 3.10.0-957.1.3.el7.x86_64 #1 SMP Thu Nov 15 17:36:42 UTC 2018 x86_64
-#   MESSAGE: xdlib_openPakLib: failed to seek end of bfp of library 'worklib'
-# -----------------------------------------------------------------
-# 
-# csi-ncvlog - CSI: Cadence Support Investigation, recording details
-# csi-ncvlog - CSI: investigation complete took 0.000 secs, send this file to Cadence Support
-
-
-
-
+exit(0)
